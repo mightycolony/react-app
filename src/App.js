574a4e8aec6e490263f9f287e0cf797abc2bd24c
root <root@karthi-9694.csez.zohocorpin.com>
2021-12-06 19:55:35 +0530

    new feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload loosy .. maruthu maruthu....ohhh ohhh
+          Edit <code>src/App.js</code> and save to reload loosy .. maruthu maruthu....ohhh ohhh...changes after feature branch
         </p>
         <a
           className="App-link"
